2b0a92ca8202b828aa3c335a5bf6249b1423952b
Rafael Mews Boeira <rafaelboeira3@gmail.com>
2025-05-13 12:07:48 -0300

    Learning Stored Procedures

---
+++ SQL/Stored_Procedures.sql
@@ -0,0 +1,31 @@
+CREATE PROCEDURE large_salaries()
+SELECT * 
+FROM employee_salary
+WHERE salary >= 50000;
+
+CALL large_salaries();
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries2()
+BEGIN
+	SELECT * 
+	FROM employee_salary
+	WHERE salary >= 50000;
+    SELECT *
+    FROM employee_salary
+    WHERE salary >= 10000;
+END $$
+DELIMITER ;
+
+CALL large_salaries2()
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries3(employee_id INT)
+BEGIN
+	SELECT * 
+	FROM employee_salary
+	WHERE employee_salary.employee_id = employee_id;
+END $$
+DELIMITER ;
+
+CALL large_salaries3(2)
